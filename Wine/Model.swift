1bdc052500c20116e2ebff43dc72d725c26df564
Galina Zbitneva <galinaknurenko@gmail.com>
2021-08-06 11:50:09 +0700

    JSON data added

--- Wine/Model.swift
+++ Wine/Model.swift
@@ -10,39 +10,63 @@ import UIKit
 
 
 struct Response: Codable {
-   var winery: String
-   var wine: String
+   var winery: String?
+   var wine: String?
    var rating: Rating
-   var location: String
-   var image: String
-   var id: Int
+   var location: String?
+   var image: String?
+   var id: Int?
    
 }
 
 struct Rating: Codable {
-    var average: Float
-    var reviews: String
+    var average: String?
+    var reviews: String?
 }
 
 
 
 class Wine {
     
+    var Wine: [Response] = []
+    var redWine: [Response] = []
+    var whiteWine: [Response] = []
+    var sparklingWine: [Response] = []
+    var roseWine: [Response] = []
     
-    var redWine: Response?
-    var whiteWine: Response?
-    var sparklingWine: Response?
-    var roseWine: Response?
-    
-    let redWineURL = URL(string: "https://api.sampleapis.com/wines/reds")
-    let whiteWineURL = URL(string: "https://api.sampleapis.com/wines/whites")
-    let sparklingWhineURL = URL(string: "https://api.sampleapis.com/wines/sparkling")
-    let roseWhineURL = URL(string: "https://api.sampleapis.com/wines/rose")
-    
-    
+    let redWineURL = "https://api.sampleapis.com/wines/reds"
+    let whiteWineURL = "https://api.sampleapis.com/wines/whites"
+    let sparklingWhineURL = "https://api.sampleapis.com/wines/sparkling"
+    let roseWhineURL = "https://api.sampleapis.com/wines/rose"
     
     
     
-    
+    func getWine(from url: String){
+
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { [self]data, response, error in
+            guard let data = data , error == nil else {
+                print("something went wrong")
+                return
+            }
+             //значит получили данные
+            var result: [Response]?
+            
+            do{
+                result = try JSONDecoder().decode([Response].self, from: data)
+                
+            }
+            catch{
+                print("faild to convert")
+            }
+            guard let json = result else {
+                return
+            }
+            
+            Wine = json
+          
+        })
+        task.resume()
+       
+    }
     
 }
